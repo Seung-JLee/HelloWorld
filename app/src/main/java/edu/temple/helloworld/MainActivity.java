5d7449d76699c8b9ce181b377a59fc0c3e9dcd5a
SJLee810 <dbsldkdl810@gmail.com>
2019-09-18 02:08:35 -0400

    New Update: Button now changes TextView when button is clicked

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,10 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button;
+
+        button = (Button) findViewById(R.id.firstButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView view = (TextView) findViewById(R.id.textView);
+                view.setText("You have clicked the button");
+            }
+        });
     }
 }
